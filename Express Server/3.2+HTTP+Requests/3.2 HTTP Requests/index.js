bc7ffb38a4f6a5d37361fb9e06796026b8cd114d
VedantTyagi2004 <128459412+VedantTyagi2004@users.noreply.github.com>
2026-08-04 21:04:57 +0530

    Update index.js

--- Express Server/3.2+HTTP+Requests/3.2 HTTP Requests/index.js
+++ Express Server/3.2+HTTP+Requests/3.2 HTTP Requests/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 app.get("/",(req , res)=>{
-    res.send("<h2>Hello World!<h2>");
+    res.send("<h2>Hello World!!!!!<h2>");
 });
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');

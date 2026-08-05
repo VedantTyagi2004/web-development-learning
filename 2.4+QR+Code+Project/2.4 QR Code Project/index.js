430ba674254d660263e7e30ad031e17a80ed5ae2
VedantTyagi2004 <vedanttyagi10@gmail.com>
2026-08-05 20:16:49 +0530

    yes

--- 2.4+QR+Code+Project/2.4 QR Code Project/index.js
+++ 2.4+QR+Code+Project/2.4 QR Code Project/index.js
@@ -19,9 +19,9 @@ fs.writeFile("URL.txt",url,(err)=>{
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      
     } else {
-      // Something else went wrong
+    
     }
   });
 
--- Express Server/3.2+HTTP+Requests/3.2 HTTP Requests/index.js
+++ Express Server/3.2+HTTP+Requests/3.2 HTTP Requests/index.js
@@ -6,3 +6,12 @@ app.get("/",(req , res)=>{
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
+<<<<<<< HEAD
+=======
+app.get("/about",(req , res)=>{
+    res.send("<h2>You are on about page<h2>");
+});
+app.get("/contact",(req , res)=>{
+    res.send("<h2>You are on contact page<h2>");
+});
+>>>>>>> f088433 (yes)

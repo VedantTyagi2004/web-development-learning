fb9c5211df5ed49fd0692a6af6f0ab554d446f7e
VedantTyagi2004 <vedanttyagi10@gmail.com>
2026-08-09 18:46:52 +0530

    morgan and custom middleware

--- Express Server/3.4+Middleware/3.4 Middleware/index3.js
+++ Express Server/3.4+Middleware/3.4 Middleware/index3.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { dirname } from "path" // these three are important to access to data  being added to form
+import { fileURLToPath } from "url";
+import bodyParser  from "body-parser"
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
+app.use(bodyParser.urlencoded({extended:true}));
+const logger = (req,res,next)=>{
+  console.log(req.url);
+  next();
+};
 app.use(logger);
 
 app.get("/", (req, res) => {

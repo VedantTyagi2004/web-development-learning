c71f2eac2cbfaee5defb37b82311dcce7a760e7e
VedantTyagi2004 <vedanttyagi10@gmail.com>
2026-08-10 20:16:22 +0530

    m

--- Express Server/3.4+Middleware/3.4 Middleware/index3.js
+++ Express Server/3.4+Middleware/3.4 Middleware/index3.js
@@ -9,6 +9,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 const logger = (req,res,next)=>{
   console.log(req.url);
+   console.log(req.method);
   next();
 };
 app.use(logger);

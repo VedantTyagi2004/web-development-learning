2dc6db22ab3b825da963341af333ff2170e51f44
VedantTyagi2004 <vedanttyagi10@gmail.com>
2026-08-24 21:05:32 +0530

    react practice

---
+++ react/jsx-code-challenge-forked/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>This is a h1</h1>
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
